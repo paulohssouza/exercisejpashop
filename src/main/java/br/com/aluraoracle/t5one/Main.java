b72c97a807e2a1e5ab4472b499a2e54d767d2d5b
phbarbudo <paulohss.souza@gmail.com>
2023-08-01 09:12:45 -0300

    Creating an entity and using annotations to map the database table.

---
+++ src/main/java/br/com/aluraoracle/t5one/Main.java
@@ -0,0 +1,4 @@
+package br.com.aluraoracle.t5one;
+
+public class Main {
+}
---
+++ src/main/java/br/com/aluraoracle/t5one/model/Product.java
@@ -0,0 +1,48 @@
+package br.com.aluraoracle.t5one.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tbproduct")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
